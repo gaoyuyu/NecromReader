128f0f31dd437776cabdf079ee52d5995be678af
gaoyuyu <740514999@qq.com>
2017-12-09 22:47:55 +0800

    取消瀑布流

--- app/src/main/java/com/gaoyy/necromreader/adapter/PhotoListAdapter.java
+++ app/src/main/java/com/gaoyy/necromreader/adapter/PhotoListAdapter.java
@@ -1,7 +1,6 @@
 package com.gaoyy.necromreader.adapter;
 
 import android.support.annotation.Nullable;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
@@ -11,7 +10,6 @@ import com.gaoyy.necromreader.api.bean.PhotoInfo;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * Created by gaoyy on 2017/3/19 0019.
@@ -29,16 +27,6 @@ public class PhotoListAdapter extends BaseQuickAdapter<PhotoInfo.ResultsBean, Ba
     {
         helper.getView(R.id.item_photo_cardview).setTag(item);
         ImageView iv = helper.getView(R.id.item_photo_img);
-
-        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) iv.getLayoutParams();
-
-        //设置随机数
-        Random random = new Random();
-        //将高度修改为传入的随机高度
-        params.height = random.nextInt(200) + 200;
-
-        //设置修改参数
-        iv.setLayoutParams(params);
 
         Picasso.with(mContext)
                 .load(item.getUrl())
